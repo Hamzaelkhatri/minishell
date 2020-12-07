df2ac885a41a60f586fa880b926d6425a18bb74d
sqatim <58560266+sqatim@users.noreply.github.com>
2020-12-07 12:52:30 +0100

    Update read_line.c

--- srcs/read_line.c
+++ srcs/read_line.c
@@ -1,5 +1,4 @@
 #include "minishell.h"
-
 int var_glob = 0; /* hadi zadtha ha9ash fach kanktab shi haja o kandir ctrl + d o n annuli dakshi li ktabt b ctrl + c 
                     makat3awdsh tkhdam ctrl + d*/
 
@@ -11,16 +10,6 @@ int read_line(t_key *key, char **line)
     // line = malloc(sizeof(char));
     *line = malloc(sizeof(char) * BUFFER_SIZE);
     ft_bzero(*line, BUFFER_SIZE);
-=======
-
-int     read_line(t_key *key,char **line)
-{
-    int     ret;
-    ret = 0;
-     
-    // line = malloc(sizeof(char));
-    *line = malloc(sizeof(char) * BUFFER_SIZE);
-     ft_bzero(*line,BUFFER_SIZE);
     if (!line)
     {
         write(1, "bash$> allocation error\n", 30);
@@ -29,7 +18,9 @@ int     read_line(t_key *key,char **line)
     if ((ret = read(0, *line, BUFFER_SIZE)) == -1)
     {
         if (line[BUFFER_SIZE] == 0)
+        {
             exit(0);
+        }
         else
             exit(1);
     }
@@ -97,21 +88,6 @@ void execute_cmd(t_cmd *cmd)
     {
         cmd->echo = 0;
     }
-    int a;
-  
-	if(sig == SIGINT)
-	{
-		signal(SIGINT, sigint_handler);
-		write(1, "\nbash$ ", 7);
-		a = fork();
-		if (!a)//a != 0
-		{
-			char *cmd[] = {"/bin/stty", 0 };
-			char *cmd_args[] = { "stty", "-echoctl", 0 };
-			char *env[] = {" ", "env", 0};
-			execve(cmd[0], cmd_args, env);
-		}
-	}
 }
 
 void promp_bash(t_cmd *cmd, t_key *key, int ret, char **line)
@@ -145,6 +121,86 @@ void promp_bash(t_cmd *cmd, t_key *key, int ret, char **line)
     // printf("|cmd->echo ==> %d|\t|key->cntrd ==> %d|\n", cmd->echo, key->cntrd);
 }
 
+char **delete_quote(char **line)
+{
+    int i;
+    int j;
+    int index;
+    char wich;
+
+    i = 0;
+    while (line[i])
+    {
+        j = 0;
+        index = 0;
+        while (line[i][j])
+        {
+            while (line[i][j] != 34 && line[i][j] != 39 && line[i][j])
+                line[i][index++] = line[i][j++];
+            if ((line[i][j] == 34 || line[i][j] == 39) && line[i][j])
+            {
+                wich = (line[i][j] == 34) ? 34 : 39;
+                j++;
+                while (line[i][j] != wich && line[i][j])
+                    line[i][index++] = line[i][j++];
+                if (line[i][j] == '\0')
+                    line[i][index] = '\0';
+                else
+                    j++;
+            }
+            if (line[i][j] == '\0')
+                line[i][index] = '\0';
+        }
+        i++;
+    }
+    return (line);
+}
+
+bool check_quote(char *line)
+{
+    int i;
+    int check;
+    char wich;
+
+    check = 0;
+    i = 0;
+    while (line[i])
+    {
+        if (line[i] == 34 || line[i] == 39)
+        {
+            wich = (line[i] == 34) ? 34 : 39;
+            i++;
+            check = 0;
+            while (line[i])
+            {
+                if (line[i] == wich)
+                {
+                    check = 1;
+                    break;
+                }
+                i++;
+            }
+            if (check != 1)
+                return (false);
+        }
+        if (line[i])
+            i++;
+    }
+    return (true);
+}
+
+void ft_nhaydo_nl(char **tab)
+{
+    int count;
+    int len;
+
+    count = ft_2strlen(tab);
+    // printf("|count ==> %d|\n", count);
+    len = ft_strlen(tab[count - 1]);
+    // printf("|len ==> %d|\n", len);
+    tab[count - 1][len - 1] = '\0';
+}
+
 void check_cmd(t_cmd *cmd, char **line, t_key *key, int ret)
 {
     int i;
@@ -153,23 +209,37 @@ void check_cmd(t_cmd *cmd, char **line, t_key *key, int ret)
     i = 1;
     if (ft_strnstr(*line, "echo", 4))
     {
-        tab = ft_space_split(*line);
-        cmd->echo = 1;
-        if (ft_str_in_str(tab[0], "echo\n"))
-            write(1, "\n", 1);
-        else
+        if (check_quote(*line))
         {
-            if (ft_2strlen(tab) == 2)
-                ft_putstr_fd(tab[1], 1);
+            tab = ft_space_split(*line);
+            tab = delete_quote(tab);
+            cmd->echo = 1;
+            if (ft_str_in_str(tab[0], "echo\n"))
+                write(1, "\n", 1);
             else
             {
-                while (i < ft_2strlen(tab))
+                if (tab[1] == 0)
+                    key->cntrd = 1;
+                else
                 {
-                    ft_putstr_fd(tab[i++], 1);
-                    if (i < ft_2strlen(tab))
-                        write(1, " ", 1);
+                    if (ft_strnstr(tab[1], "-n", 2) || ft_strnstr(tab[1], "-n\n", 3))
+                    {
+                        ft_nhaydo_nl(tab);
+                        i++;
+                    }
+                    while (i < ft_2strlen(tab))
+                    {
+                        ft_putstr_fd(tab[i++], 1);
+                        if (i < ft_2strlen(tab))
+                            write(1, " ", 1);
+                    }
                 }
             }
+        }
+        else
+        {
+            ft_putstr_fd("check quotes", 1);
+            exit(0);
         }
     }
     else
@@ -193,105 +263,9 @@ void loop_shell(t_key *key, t_cmd *cmd)
     {
         ret = 0;
         ret = read_line(key, &line);
-        // ft_putnbr_fd(ret,1);
         check_cmd(cmd, &line, key, ret);
         var_glob = 0;
-	i = 1;
-	if(ft_str_in_str("echo",tab[0]))
-	{
-		if(ft_2strlen(tab) == 1)
-			write(1,"\n",1);
-		else
-		{
-			if(ft_2strlen(tab) == 2)
-				ft_putstr_fd(tab[1],1);
-			else
-			{
-				while(i < ft_2strlen(tab))
-				{
-					ft_putstr_fd(tab[i++],1);
-					if(i < ft_2strlen(tab))
-					write(1," ",1);
-				}
-			}
-		}
-	}
-}
-
-int search_cmd(t_cmd *cmd)
-{
-    if(cmd->echo!=0 || cmd->cd!=0 || cmd->env != 0 ||
-        cmd->exit != 0 || cmd->export != 0 || cmd->pwd != 0)
-    {
-        return (0);
-    }
-    return (1);
-}
-
-void execute_cmd(t_cmd *cmd)
-{
-    if(cmd->echo != 0)
-    {
-        
-        cmd->echo=0;
-    }
-}
-
-void promp_bash(t_cmd *cmd,t_key *key,int ret,char **line)
-{
-    if(ret == 0 && key->cntrd !=1)
-    {
-        ft_putstr_fd("exit", 1);
-        exit (1);
-    }
-    if(ft_strrchr(*line, '\n') && search_cmd(cmd))
-    {
-        write(1, "bash$ ", 6);
-        key->cntrd = 0;
-    }
-    else if(ft_strrchr(*line, '\n'))
-    {
-
-        ft_putstr_fd("\nbash$ ",1);
-        key->cntrd = 0;
-    }    
-    if(ret > 0 && !ft_strrchr(*line, '\n'))
-       key->cntrd = 1;
-}
-
-void check_cmd(t_cmd *cmd,char **line,t_key *key,int ret)
-{
-    int i;
-    
-    i = 0;
-        if(ft_str_in_str(*line,"echo"))
-        {   
-            cmd->echo =1;
-        }
-        else
-        {
-        }
-        
-        promp_bash(cmd,key,ret,line);
-}
-
-
-void	loop_shell(t_key *key,t_cmd *cmd)
-{
-    char	*line;
-    int     ret;
-    int		status;
-    int		cntrd;
-    
-    signal(SIGINT, sigint_handler);
-    status = 1;
-    cntrd = 0;
-    ft_putstr_fd("bash$ ",1);//\e[32;40m
-    while (status)
-    {
-        ret = read_line(key,&line);
-        // ft_putnbr_fd(ret,1);
-        check_cmd(cmd,&line,key,ret);
         free(line);
     }
 }
+
