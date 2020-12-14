df63360297ca200ed2ec35048bde8e8381a7161b
Amine Haddad <ahaddad@e2r3p11.1337.ma>
2020-12-14 19:02:21 +0100

    adding export

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,26 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: helkhatr < helkhatr@student.1337.ma>       +#+  +:+       +#+        */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 02:17:32 by helkhatr          #+#    #+#             */
-/*   Updated: 2020/12/09 14:42:46 by helkhatr         ###   ########.fr       */
+/*   Updated: 2020/12/14 19:02:06 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int		main(int argc, char **argv,char **env)
+
+int count_line(char **env)
+{
+	int i = 0;
+	if (!env)
+		return (0);
+	while (env[i])
+		i++;
+	return (i);
+}
+
+void	export_cmd(char *name,char **env)
+{
+	int i = 0;
+	int line = count_line(env);
+	printf("{%d}",line);
+	env[line] = name;
+	env[line+1] = NULL;
+}
+int main(int argc, char **argv,char **env)
 {
 	t_path path;
 	t_key key;
 	t_cmd cmd;
+	// char *env_tmp[33];
 	char *s;
-	
 	init(&path,&key,&cmd);
 	path.env->fullenv = env;
-	cd_cmd("/goinfre/djejd",&path);
+	export_cmd("myvar=300",path.env->fullenv);
+	// printf("\nHERE ; [%s]\n",search_env(path.env->fullenv,"PWD"));
+	int i = 0;
+	int line = count_line(env);
+	// while (i < li)
+	// {
+	// 	puts(env[i]);
+	// 	i++;
+	// }
+	
+	// printf("{%d}",count_line(env));
+	// cd_cmd("srcs",&path);
 	get_directory(&path);
-	getprogramme(&path,"ls");
+	// loop_shell(&cmd,&path);
+	// getprogramme(&path,"ls");
 	return (0);
 }
