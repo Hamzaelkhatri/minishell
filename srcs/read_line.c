bf981118915d342b12ae3a0ed7bae88087394f9f
hamza <hamzaelkhatri@gmail.com>
2021-02-04 19:44:34 -0500

    fix cntrl-D

--- srcs/read_line.c
+++ srcs/read_line.c
@@ -1,6 +1,8 @@
 #include "minishell.h"
 #include <setjmp.h>
-int var_glob = 0;
+int var_glob;
+int var_glob1;
+
 /* hadi zadtha ha9ash fach kanktab shi haja o kandir ctrl + d o n annuli dakshi li ktabt b ctrl + c 
 		     makat3awdsh tkhdam ctrl + d*/
 
@@ -22,6 +24,8 @@ int read_line(t_path *key, char **line)
 		else
 			exit(1);
 	}
+	// if(ret)
+	// ft_putstr_fd(*line,2);
 	return (ret);
 }
 
@@ -51,12 +55,11 @@ void sigint_handler(int sig)
 		if (var_glob)
 			ft_putstr_fd("\n\e[1;32mbash$ \e[0;37m", 2);
 		var_glob = 111;
+		var_glob1 = 0;
 		avoid_cntc();
 	}
 	if (sig == SIGQUIT)
-	{
 		var_glob = 111;
-	}
 }
 
 void promp_bash(t_path *path, int ret, char **line)
@@ -90,63 +93,89 @@ void loop_shell(t_path *path)
 {
 	char *line;
 	int ret;
-	int status;
-	int salam;
-	t_list_cmd *cmd = NULL;
-	struct stat buff;
 	int i;
+	char *lines;
+	t_list_cmd *cmd = NULL;
 	int check;
 
+	i = 0;
 	if (signal(SIGINT, sigint_handler) == SIG_ERR)
 		ft_putstr_fd("\n\e[1;31m can't catch cnrtl-C", 2);
 	if (signal(SIGQUIT, sigint_handler) == SIG_ERR)
 		ft_putstr_fd("\n\e[1;31m can't catch cntrl-\\", 2);
-	status = 0;
 	var_glob = 0;
 	path->dollar = 0;
-	i = 0;
+	lines = NULL;
 	bash_promp();
-	while (!status)
+	var_glob1 = 0;
+	while (1)
 	{
+		i = 0;
 		if (check > 0)
 			path->dollar = check;
+
 		check = 0;
-		i = 0;
 		ret = 0;
 		ret = read_line(path, &line);
-		if (line[0] == '\0')
+		if (line[0] == '\0' && !var_glob1)
 		{
 			ft_putendl_fd("exit", 1);
 			exit(EXIT_SUCCESS);
 		}
-		else if (line[0] == '\n')
-			check = -1;
-		path->cmds = line;
-		if (check != 0)
-			while (line[i] && line[i] == ' ')
-				i++;
-		if (line[i] != '\n')
+		if (line[ret - 1] != '\n')
 		{
-			path->cmds = ft_strtrim(path->cmds, "\n");
-			check = ft_check_line(path->cmds);
+			if (lines)
+				lines = ft_strjoin(lines, line);
+			else
+				lines = ft_strdup(line);
+			var_glob1 = 1;
 		}
-		if (!check)
+		else if (lines)
+		{
+			lines = ft_strjoin(lines, line);
+			line = ft_strdup(lines);
+			free(lines);
+			lines = NULL;
+			var_glob1 = 0;
+		}
+		if (line[0] == '\n')
+		{
+			check = -1;
+			var_glob1 = 0;
+		}
+		if (!var_glob1)
 		{
 			var_glob = 0;
-			cmd = add_list_cmd(cmd);
-			cmd->line = ft_strdup(path->cmds);
-			parse_list_command(cmd, cmd->line);
-			ft_strdel(&path->cmds);
-			sort(cmd);
-			quotes(cmd);
-			// check_scommand(cmd);
-			commande_effect(cmd, path);
-			// print(cmd);
-			free_lcommand(&cmd);
+			path->cmds = line;
+			if (check != 0)
+				while (line[i] && line[i] == ' ')
+					i++;
+			if (line[i] != '\n')
+			{
+				path->cmds = ft_strtrim(path->cmds, "\n");
+				check = ft_check_line(path->cmds);
+			}
+			if (!check)
+			{
+				ft_putstr_fd(line, 1);
+				cmd = add_list_cmd(cmd);
+				cmd->line = ft_strdup(path->cmds);
+				parse_list_command(cmd, cmd->line);
+				ft_strdel(&path->cmds);
+				sort(cmd);
+				quotes(cmd);
+				commande_effect(cmd, path);
+				free_lcommand(&cmd);
+			}
+			if (var_glob == 111 && line[0] != '\n')
+				ft_putendl_fd("", 1);
+			bash_promp();
+			var_glob = 11;
 		}
-		if (var_glob == 111 && line[0] != '\n')
-			ft_putendl_fd("", 1);
-		bash_promp();
-		var_glob = 11;
+		if (line)
+		{
+			free(line);
+			line = NULL;
+		}
 	}
 }
